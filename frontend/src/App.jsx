1e2d77d3ad2cf8b0c401091d7b6f3a7ed17b6d5e
Aaron Dudue <duduzilesibanda99@gmail.com>
2022-04-02 10:20:55 -0700

    first online commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css';
+import {Sidebar} from './components'
+
+
+const App = () => {
+  return (
+    <>
+      <Sidebar />
+      <div>Main Content</div>
+    </>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,210 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div>
+      <div className="logo">
+        <div className="logo-image">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="430.03"
+            height="413.989"
+            viewBox="0 0 430.03 413.989"
+          >
+            <g
+              id="Group_1"
+              data-name="Group 1"
+              transform="translate(-436.865 -256.306)"
+            >
+              <path
+                id="Path_3"
+                data-name="Path 3"
+                d="M690.728,297.712c0,88.247-52.462,119.061-117.177,119.061S456.374,385.959,456.374,297.712,573.551,97.2,573.551,97.2,690.728,209.464,690.728,297.712Z"
+                transform="translate(100.065 159.107)"
+                fill="#f2f2f2"
+              />
+              <path
+                id="Path_4"
+                data-name="Path 4"
+                d="M549.583,280.151l1.1-67.6L596.4,128.92l-45.542,73.026.494-30.4,31.506-60.506-31.375,52.463h0l.888-54.668L586.1,60.668l-33.6,39.574L553.06,0l-3.487,132.7.286-5.474-34.3-52.5,33.751,63.012-3.2,61.055-.1-1.62L506.476,141.92,545.9,202.9l-.4,7.636-.072.115.033.627-8.109,154.9h10.833l1.3-80.01,39.326-60.827Z"
+                transform="translate(122.18 262.814)"
+                fill="#3f3d56"
+              />
+              <path
+                id="Path_13"
+                data-name="Path 13"
+                d="M422.419,363.093l-6.58-10.966,61.413-52.639H635.168v15.353l-57.026,61.413-65.8,13.159Z"
+                transform="translate(133.859 176.596)"
+                fill="#3f3d56"
+              />
+              <path
+                id="Path_14"
+                data-name="Path 14"
+                d="M693.47,329.654V295.338H672.794v10.821l-6.509-7.4L613.647,351.4l-8.773,8.772-10.967,43.866,19.74,48.252H714.538V353.6Z"
+                transform="translate(102.742 177.321)"
+                fill="#ccc"
+              />
+              <rect
+                id="Rectangle_8"
+                data-name="Rectangle 8"
+                width="166.691"
+                height="100.892"
+                transform="translate(549.698 528.723)"
+                fill="#f2f2f2"
+              />
+              <rect
+                id="Rectangle_9"
+                data-name="Rectangle 9"
+                width="35.093"
+                height="30.706"
+                transform="translate(658.266 560.526)"
+                fill="#3f3d56"
+              />
+              <rect
+                id="Rectangle_10"
+                data-name="Rectangle 10"
+                width="30.706"
+                height="70.185"
+                transform="translate(613.303 559.429)"
+                fill="#ccc"
+              />
+              <path
+                id="Path_15"
+                data-name="Path 15"
+                d="M447.6,411.448V400.482h-13.16v10.966H447.6Z"
+                transform="translate(130.608 158.947)"
+                fill="#3f3d56"
+              />
+              <path
+                id="Path_16"
+                data-name="Path 16"
+                d="M446.506,424.4H434.443v10.967H447.6V424.4Z"
+                transform="translate(130.608 154.767)"
+                fill="#3f3d56"
+              />
+              <rect
+                id="Rectangle_11"
+                data-name="Rectangle 11"
+                width="13.16"
+                height="10.967"
+                transform="translate(586.984 559.429)"
+                fill="#3f3d56"
+              />
+              <rect
+                id="Rectangle_12"
+                data-name="Rectangle 12"
+                width="13.16"
+                height="10.967"
+                transform="translate(586.984 579.168)"
+                fill="#3f3d56"
+              />
+              <path
+                id="Path_17"
+                data-name="Path 17"
+                d="M672.182,412.777V401.811H659.022v10.966h13.159Z"
+                transform="translate(91.363 158.715)"
+                fill="#3f3d56"
+              />
+              <path
+                id="Path_18"
+                data-name="Path 18"
+                d="M671.085,425.73H659.022V436.7h13.159V425.73Z"
+                transform="translate(91.363 154.535)"
+                fill="#3f3d56"
+              />
+              <rect
+                id="Rectangle_13"
+                data-name="Rectangle 13"
+                width="13.16"
+                height="10.967"
+                transform="translate(772.317 560.526)"
+                fill="#3f3d56"
+              />
+              <rect
+                id="Rectangle_14"
+                data-name="Rectangle 14"
+                width="13.16"
+                height="10.967"
+                transform="translate(772.317 580.265)"
+                fill="#3f3d56"
+              />
+              <rect
+                id="Rectangle_22"
+                data-name="Rectangle 22"
+                width="1.651"
+                height="65.195"
+                transform="translate(832.959 564.476)"
+                fill="#3f3d56"
+              />
+              <path
+                id="Path_25"
+                data-name="Path 25"
+                d="M802.358,505.139c.1,22.8-15.934,41.352-15.934,41.352s-16.209-18.406-16.314-41.2,15.934-41.352,15.934-41.352S802.254,482.342,802.358,505.139Z"
+                transform="translate(47.484 67.645)"
+                fill="#0f6274"
+              />
+              <rect
+                id="Rectangle_23"
+                data-name="Rectangle 23"
+                width="1.651"
+                height="65.195"
+                transform="translate(532.568 562.825)"
+                fill="#3f3d56"
+              />
+              <path
+                id="Path_26"
+                data-name="Path 26"
+                d="M438.358,503.139c.1,22.8-15.934,41.352-15.934,41.352s-16.209-18.406-16.314-41.2,15.934-41.352,15.934-41.352S438.254,480.342,438.358,503.139Z"
+                transform="translate(111.093 67.995)"
+                fill="#0f6274"
+              />
+              <path
+                id="Path_27"
+                data-name="Path 27"
+                d="M438.358,503.139c.1,22.8-15.934,41.352-15.934,41.352s-16.209-18.406-16.314-41.2,15.934-41.352,15.934-41.352S438.254,480.342,438.358,503.139Z"
+                transform="translate(111.093 67.995)"
+                opacity="0.1"
+              />
+              <rect
+                id="Rectangle_24"
+                data-name="Rectangle 24"
+                width="1.651"
+                height="65.195"
+                transform="translate(511.112 562.825)"
+                fill="#3f3d56"
+              />
+              <path
+                id="Path_28"
+                data-name="Path 28"
+                d="M412.358,503.139c.105,22.8-15.934,41.352-15.934,41.352s-16.209-18.406-16.314-41.2,15.934-41.352,15.934-41.352S412.254,480.342,412.358,503.139Z"
+                transform="translate(115.636 67.995)"
+                fill="#0f6274"
+              />
+              <rect
+                id="Rectangle_25"
+                data-name="Rectangle 25"
+                width="391.31"
+                height="1.651"
+                transform="translate(475.585 628.024)"
+                fill="#3f3d56"
+              />
+              <path
+                id="Path_31"
+                data-name="Path 31"
+                d="M320.815,615.107s5.713,5.237,4.285,5.713-6.666,6.189-7.618,6.666c-.852.426-10,3.333-8.57,0a12.524,12.524,0,0,1,4.761-5.237l.952-5.713Z"
+                transform="translate(128.104 41.228)"
+                fill="#f2f2f2"
+              />
+            </g>
+          </svg>
+        </div>
+        <h3 className="logo-text">DEMO Farmer</h3>
+      </div>
+      <nav className="navigation-bar">
+        ul.nav-links
+      </nav>
+    </div>
+  );
+};
+
+export default Sidebar;
